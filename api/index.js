40bc81de02e1edb56de35c33ddfb8fd33a37c3fe
Sithija Shehara <nsithijashehara@gmail.com>
2021-12-28 19:52:45 +0530

    added posts section

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -23,5 +24,6 @@ app.use(morgan("common"));
 // routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8000, () => console.log("Backend is running !"));
