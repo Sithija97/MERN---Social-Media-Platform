d15f2c0a07d6a0003c23b409f415146c47b4e531
Sithija Shehara <nsithijashehara@gmail.com>
2021-12-29 22:30:03 +0530

    implemented rightbar

--- client/src/components/rightbar/rightbar.jsx
+++ client/src/components/rightbar/rightbar.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">right</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/5.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Cena</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
